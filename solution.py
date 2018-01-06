5f7743f785d63be55dee4c11d59e72f08fd5282b
Stephen <stephen@Stephens-MacBook-Pro.local>
2018-01-06 10:22:57 -0800

    First solution draft. Some data analyses with aggregations.

---
+++ solution.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+
+TRAIN_PATH = "data/train.csv"
+TEST_PATH = "data/test.csv"
+
+train_df = pd.read_csv(TRAIN_PATH)
+test_df = pd.read_csv(TEST_PATH)
+
+#####################
+# Understand the data
+print("The column names are %s" % (train_df.columns.values))
+# See first few rows.
+train_df.head()
+# Schema of the dataframe
+train_df.info()
+# Stats like means, etc for each column.
+# Can also see missing data from 'count'
+# Need 'all' so categorical columns like Name is included.
+train_df.describe(include='all')
+
+# For each feature, check distribution against label,
+# to see if feature is promising.
+# Each class, percent survival.
+train_df[['Pclass', 'Survived']] \
+    .groupby(['Pclass'], as_index=False) \
+    .mean() \
+    .sort_values(by='Survived', ascending=False)
+# Each gender, percent survival.
+train_df[['Sex', 'Survived']] \
+    .groupby(['Sex'], as_index=False) \
+    .mean() \
+    .sort_values(by='Survived', ascending=False)
+
