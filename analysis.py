73a5d24ee46060418255dc21b039a83d619e0360
Stephen <stephen@Stephens-MacBook-Pro.local>
2018-01-07 13:31:44 -0800

    Add IsAlone feature, and refactored. Everything is in methods except for main(). Also, printDiscreteStats will be a useful debugging tool.

--- analysis.py
+++ analysis.py
@@ -12,61 +12,79 @@ import matplotlib.pyplot as plt
 TRAIN_PATH = "data/train.csv"
 TEST_PATH = "data/test.csv"
 
-train_df = pd.read_csv(TRAIN_PATH)
-test_df = pd.read_csv(TEST_PATH)
+#####################
+# Analysis tools
+
+def printDiscreteStats(df, colName):
+    """
+    Print tally for column, and see relationship against label.
+    The label is hardcoded to 'Survived'.
+    @param df (dataframe)
+    @param colName (string) - a feature column in 'df'.
+    """
+    print(df[[colName, 'Survived']] \
+        .groupby([colName]) \
+        .agg(['mean', 'count']) \
+        .sort_values(
+                by=[('Survived', 'mean')],
+                ascending=False))
 
 #####################
 # Understand the data
-print("The column names are {0}" % (train_df.columns.values))
-# See first few rows.
-train_df.head()
-# Schema of the dataframe
-train_df.info()
-# Stats like means, etc for each column.
-# Can also see missing data from 'count'
-# Need 'all' so categorical columns like Name is included.
-train_df.describe(include='all')
 
-# For each feature, check distribution against label,
-# to see if feature is promising.
-# Each class, percent survival.
-train_df[['Pclass', 'Survived']] \
-    .groupby(['Pclass'], as_index=False) \
-    .mean() \
-    .sort_values(by='Survived', ascending=False)
-# Each gender, percent survival.
-train_df[['Sex', 'Survived']] \
-    .groupby(['Sex'], as_index=False) \
-    .mean() \
-    .sort_values(by='Survived', ascending=False)
-
-# Plot histogram
-g = sea.FacetGrid(train_df, col='Survived')
-g.map(plt.hist, 'Age', bins=20)
-
-# Break histogram down with another dimension.
-g = sea.FacetGrid(train_df, row='Pclass', col='Survived')
-# Alpha = transparency
-g.map(plt.hist, 'Age', alpha=.5, bins=20)
-
-# Point plot. Show survival rate for [embarkations, pclass, gender]
-# This means there's a chart / row per embarkation
-g = sea.FacetGrid(train_df, row = 'Embarked')
-# x = Pclass, y = Survived, breakdown = Sex
-# Without palette, the color difference is not that striking
-g.map(sea.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
-# So the gender legend shows up
-g.add_legend()
-
-# Bar chart.
-g = sea.FacetGrid(train_df, row='Embarked', col='Survived')
-# If ci (confidence interval) exists, there is a vertical line on every bar.
-g.map(sea.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
+def printPrelimAnalysis(df):
+    """
+    Preliminary analysis. Just visualizations before we wrangle anything.
+    @param df (dataframe)
+    """
+    print("The column names are {0}" % (df.columns.values))
+    # See first few rows.
+    df.head()
+    # Schema of the dataframe
+    df.info()
+    # Stats like means, etc for each column.
+    # Can also see missing data from 'count'
+    # Need 'all' so categorical columns like Name is included.
+    df.describe(include='all')
+    
+    # For each feature, check distribution against label,
+    # to see if feature is promising.
+    # Each class, percent survival.
+    df[['Pclass', 'Survived']] \
+        .groupby(['Pclass'], as_index=False) \
+        .mean() \
+        .sort_values(by='Survived', ascending=False)
+    # Each gender, percent survival.
+    df[['Sex', 'Survived']] \
+        .groupby(['Sex'], as_index=False) \
+        .mean() \
+        .sort_values(by='Survived', ascending=False)
+    
+    # Plot histogram
+    g = sea.FacetGrid(df, col='Survived')
+    g.map(plt.hist, 'Age', bins=20)
+    
+    # Break histogram down with another dimension.
+    g = sea.FacetGrid(df, row='Pclass', col='Survived')
+    # Alpha = transparency
+    g.map(plt.hist, 'Age', alpha=.5, bins=20)
+    
+    # Point plot. Show survival rate for [embarkations, pclass, gender]
+    # This means there's a chart / row per embarkation
+    g = sea.FacetGrid(df, row = 'Embarked')
+    # x = Pclass, y = Survived, breakdown = Sex
+    # Without palette, the color difference is not that striking
+    g.map(sea.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+    # So the gender legend shows up
+    g.add_legend()
+    
+    # Bar chart.
+    g = sea.FacetGrid(df, row='Embarked', col='Survived')
+    # If ci (confidence interval) exists, there is a vertical line on every bar.
+    g.map(sea.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
 
 #####################
 # Data wrangling
-
-
 def impute_age(df):
     """
     Impute missing age values.
@@ -115,14 +133,21 @@ def update_features(df):
     df['Title'] = df['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
       'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
     
+    # Add IsAlone as a feature
+    family_size_lst = df['SibSp'] + df['Parch'] + 1
+    df['IsAlone'] = 0
+    df.loc[family_size_lst == 1, 'IsAlone'] = 1
+    
     # Drop useless features
-    df = df.drop(columns=['Ticket', 'Cabin', 'PassengerId', 'Name', 'Age'])
+    df = df.drop(columns=['Age', 'Cabin', 'Name', 'Parch', 'PassengerId',
+                          'SibSp','Ticket'])
     return df
 
-# Drop features
+"""
+Main
+"""
+train_df = pd.read_csv(TRAIN_PATH)
+test_df = pd.read_csv(TEST_PATH)
 print("Before updateFeatures {0}".format(train_df.shape))
 train_df = update_features(train_df)
 print("After updateFeatures {0}".format(train_df.shape))
-# Tally with group-by.
-pd.crosstab(train_df['Title'], train_df['Sex'])
-# To groupby-count one column: train_df.groupby('AgeGroup')['AgeGroup'].count()
